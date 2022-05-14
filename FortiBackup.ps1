d0e96cfd517351d6a5800b2d2ea1eb634e2291a2
Dan Parr <dparr@granite-it.net>
2022-05-13 21:52:29 -0300

    Files Added

---
+++ FortiBackup.ps1
@@ -0,0 +1,63 @@
+﻿
+##### SETUP #####
+
+$FGFQDN = "172.16.200.254" #Used To Connect to API
+$Hostname = "FG-001" # Used in Filename of Config Backup
+$Port = "4443"
+$API_Key = "88888888nHsc33363mfHb3w7777777"
+$SavePath = "C:\ConfigBackups" # Where backup files will be Stored
+$RetentionDays = 30
+$APIUrl = "https://$FGFQDN`:$Port/api/v2/monitor/system/config/backup?scope=global&access_token=$API_Key"
+
+#If Certificate Trust Fails / Self Signed Certificate is used Set to True
+$IgnoreCertCheck = $true
+#################
+
+#Create SavePath if needed
+if (!(Test-path -path $SavePath)){md $SavePath}
+
+$Header = "
+##########################################################################
+#
+#   FortiOS Config Backup Utility
+#   
+#   Version: 1.1 / May 13 2022
+#   Author: Dan Parr / dparr@granite-it.net
+#
+##########################################################################
+"
+
+
+if ($IgnoreCertCheck){
+    add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
+
+
+clear-host
+write-host -foregroundcolor yellow  $Header
+
+# Make API Call to Fortigate
+$Response = Invoke-RestMethod -Method Get -Uri $APIUrl -ContentType "application/json" -Timeoutsec 120
+#
+
+If ($Response.Length -gt 10){
+    Write-host -foregroundcolor green "Config Successfully Collected"
+    $d = (((get-date).ToString("yyyyMMdd_HHmmss")))
+    $Response | Out-file "$SavePath\$Hostname`_$d.conf"
+
+    #Remove Saved Configs Older than the Retention Period - Keep in If Statement to prevent Schedule from clearing saved files unless it added a new one.
+    $limit = (get-date).adddays(-$RetentionDays)
+    write-host "Clearing Aged Config Backups Older than $RetentionDays Days"
+    Get-ChildItem -Path $SavePath | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# ForitOS Backup Utility: FortiBackup.ps1
+
+This is a Basic Script that will make a WebAPI call to a Fortigate firewall and Perform a Full backup of the Configuration.
+The Folder where inforamtion is saved is maintained by the RetentionDays variable.
+
+## Setup Variables
+
+- **$FGFQDN:** either the fully qualified domain name of the Fortigate or it's IP address
+- **$Hostname:** System Hostname Used in Filename of Config Backup (Should Match the Hostname of the Fortigate)
+- **$Port:** The TCP Port on Which the FortiOS API / WebUI are listening
+- **$API_Key:** The API Key Generated for the API Administrator you Configure on the Fortigate
+- **$SavePath:** The Folder Path where Backup Configs will be stored
+- **$RetentionDays:** The number of days to retain configuration backup files
+- **$IgnoreCertCheck:** If using a self-signed certificate for the management webui/API this will need to be set to $true
+
+## File Naming Format
+
+Backed up configuation files will be named in the following format: hostename_yyyyMMdd_HHmmss.conf
+This will allow for easy identification of the exact Date and Time that each backup was taken.
+
+## Backup File Retention
+
+Once the Current backup has been collected from the API and saved the script will remove any files from the SavePath folder that are older than the period of days specified in the RetentionDays variable.
